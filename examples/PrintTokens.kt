aa1cc58538b919aa9563bc2e163a7c9b03fbe9d7
leogtzr <leogutierrezramirez@gmail.com>
2018-01-21 14:21:54 -0400

    Initial code.

---
+++ examples/PrintTokens.kt
@@ -0,0 +1,21 @@
+packagde examples
+
+import me.tomassetti.minicalc.MiniCalcLexer
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.runtime.Token
+import java.io.FileInputStream
+import java.io.StringReader
+
+fun lexerForCode(code: String) = MiniCalcLexer(ANTLRInputStream(StringReader(code)))
+fun readExampleCode() = FileInputStream("examples/rectangle.mc").bufferedReader().use { it.readText() }
+
+fun main(args: Array<String>) {
+    val lexer = lexerForCode(readExampleCode())
+    var token: Token? = null
+    do {
+        token = lexer.nextToken()
+        val typeName = MiniCalcLexer.VOCABULARY.getSymbolicName(token.type)
+        val text = token.text.replace("\n", "\\n").replace("\r", "\\r").replace("\t", "\\t")
+        println("L${token.line}(${token.startIndex}-${token.stopIndex}) $typeName '$text'")
+    } while (token?.type != -1)
+}
